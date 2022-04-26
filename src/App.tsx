7b246b2bc8a2b64f27210b64fd8fe8d976d44bdf
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-26 12:28:25 +0200

    commentedout google login

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Todo from "./Model/model";
 import TodoList from "./Components/TodoList";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
-
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -55,25 +54,49 @@ const App: React.FC = () => {
   };
 
   /* sing up and sign in with google states */
-   const [name, setName] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
+/*   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser: React.FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+    const response = await fetch("http://localhost:5000/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }; */
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
         {/* sign up and sign in with google */}
         <h1>Register</h1>
-        <form >
-          <input 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          placeholder="Name"
-          
+        <form onSubmit={registerUser}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Name"
           />
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Password" />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            placeholder="Password"
+          />
+
+          <button type="submit">Submit</button>
         </form>
 
         <span className="heading">Task List</span>
